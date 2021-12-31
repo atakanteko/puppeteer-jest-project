9d99c47aaf5202f479363aa59547ad217573d618
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2021-12-31 18:35:18 +0300

    [Atakan] end-to-end test and fix capitalize var names

--- tests/end-to-end.test.js
+++ tests/end-to-end.test.js
@@ -4,44 +4,33 @@ import TopBar from '../pages/components/TopBar'
 import FeedbackPage from '../pages/FeedbackPage'
 
 describe('End-To-End Test', () => {
-  let homepage
-  let loginpage
-  let topbar
-  let feedback
+  let homePage
+  let loginPage
+  let topBar
+  let feedBack
 
   beforeAll(async () => {
     jest.setTimeout(150000);
-    homepage = new HomePage();
-    loginpage = new LoginPage();
-    topbar = new TopBar();
-    feedback = new FeedbackPage();
+    homePage = new HomePage();
+    loginPage = new LoginPage();
+    topBar = new TopBar();
+    feedBack = new FeedbackPage();
   })
 
-  it('homepage should work', async () => {
-    await homepage.visit();
+  it("should load homepage", async () => {
+    await homePage.visit();
+    await homePage.isNavbarDisplayed();
   })
 
-  it('navbar should be appeared', async ()=> {
-    await homepage.isNavbarDisplayed();
-    await topbar.isTopBarDisplayed();
-  })
-
-  it('login fail', async () => {
-    await topbar.clickSignInButton();
-    await loginpage.isLoginFormDisplayed();
-    await loginpage.login("atakan","123456");
-  })
-
-  it('feedback should work', async () => {
-    await feedback.visit();
-    await feedback.isFeedbackFormDisplayed();
-    await feedback.submitFeedback(
+  it("should submit feedback", async () => {
+    await homePage.clickFeedbackLink();
+    await feedBack.isFeedbackFormDisplayed();
+    await feedBack.submitFeedback(
       "Ata",
       "atakantekoglu@gmail.com",
       "Personel",
       "Hello Puppeteer"
     );
-    await feedback.wait(3000);
   })
 
 
